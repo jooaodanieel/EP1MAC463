5f382e72cceec7c8bc551605c45121bdac3d9915
João Daniel <jotaf.daniel@gmail.com>
2017-04-21 16:25:20 -0300

    student activity iniciada porém sugiro que seja descartado para pensar no esquema de login único

---
+++ app/src/main/java/ep1/joaofran/com/ep1/StudentActivity.java
@@ -0,0 +1,16 @@
+package ep1.joaofran.com.ep1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+
+
+        // alguma coisa
+    }
+}
