2bed12133eb4f0ffa1cf338b86d6d2e255a18b51
Victor Wichmann Raposo <vwraposo@gmail.com>
2017-04-21 15:52:12 -0300

    Post Task começo

--- app/src/main/java/tasks/PostTask.java
+++ app/src/main/java/tasks/PostTask.java
@@ -4,10 +4,12 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,6 +19,14 @@ public class PostTask extends AsyncTask<String, Void, Boolean> {
     private static final String URL_BEGIN = "http://207.38.82.139:8001/";
     private static final String TAG = "POSTTASK";
 
+    private String url;
+    private String[] params;
+
+    public PostTask (String... params) {
+        this.params = params;
+    }
+
+
     @Override
     protected Boolean doInBackground (String... url_end){
 
@@ -24,6 +34,8 @@ public class PostTask extends AsyncTask<String, Void, Boolean> {
             this.url = URL_BEGIN + url_end[0];
             Log.d(TAG, "in Background " + this.url);
             HttpURLConnection urlConnection = (HttpURLConnection) new URL(this.url).openConnection();
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream();
 
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -40,5 +52,7 @@ public class PostTask extends AsyncTask<String, Void, Boolean> {
             e.printStackTrace();
         }
     }
+
+    
 
 }
