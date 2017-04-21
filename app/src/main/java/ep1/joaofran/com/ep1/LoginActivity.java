39aa707f4c7e9cc53291bf47fe36e417f44e4ac9
João Daniel <jotaf.daniel@gmail.com>
2017-04-21 14:30:28 -0300

    HTTP request de login

--- app/src/main/java/ep1/joaofran/com/ep1/LoginActivity.java
+++ app/src/main/java/ep1/joaofran/com/ep1/LoginActivity.java
@@ -2,12 +2,32 @@ package ep1.joaofran.com.ep1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText senha;
+    private static final String TAG = "LOGIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (EditText) findViewById(R.id.etNUSP);
+        senha = (EditText) findViewById(R.id.etPassword);
+    }
+
+    public void onClick (View view) {
+        String l = login.getText().toString();
+        String p = senha.getText().toString();
+
+        Log.d(TAG, "Botão clicked");
+
+        LoginTask task = new LoginTask("login","senha");
+        task.execute("string qualquer");
     }
 }
