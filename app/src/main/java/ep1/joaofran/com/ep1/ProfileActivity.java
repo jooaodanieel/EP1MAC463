ff36ef38a37a2b669c3496a2efe471cbda68c5dc
Victor Wichmann Raposo <vwraposo@gmail.com>
2017-04-22 17:42:39 -0300

    Paginas construidas

--- app/src/main/java/ep1/joaofran/com/ep1/ProfileActivity.java
+++ app/src/main/java/ep1/joaofran/com/ep1/ProfileActivity.java
@@ -3,10 +3,16 @@ package ep1.joaofran.com.ep1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "PROFILE";
+    private ListView seminars_list;
+//    private ArrayList<Semninar> seminars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +20,5 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student);
         Log.d (TAG, "In profile activity");
 
-
-
-        // alguma coisinha
     }
 }
--- app/src/main/java/ep1/joaofran/com/ep1/SignUpActivity.java
+++ app/src/main/java/ep1/joaofran/com/ep1/SignUpActivity.java
@@ -1,12 +1,20 @@
 package ep1.joaofran.com.ep1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String TAG = "SIGNUP";
+
+    private EditText name;
+    private EditText login;
+    private EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,18 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         Log.d (TAG, "In SignUP activity");
+    }
+
+    public void signUp(View view) {
+
+        // Sign up no webserver
+
+        Intent intent = new Intent(view.getContext(), ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void linkLogin(View view) {
+        Intent intent = new Intent(view.getContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
