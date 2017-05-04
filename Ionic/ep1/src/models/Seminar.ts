93a945473e6e63812ab2a59724c8e1f64ffe48a6
Victor Wichmann Raposo <vwraposo@gmail.com>
2017-05-03 22:07:40 -0300

    Removendo Ionic

--- Ionic/ep1/src/models/Seminar.ts
+++
@@ -1,5 +0,0 @@
-export class Seminar {
-
-  constructor(public id: number, public name: string){
-  }
-}
--- Ionic/ep1/src/models/user.ts
+++
@@ -1,4 +0,0 @@
-export class User {
-  constructor(public nusp: string, public name: string, public is_student: boolean){
-  }
-}
