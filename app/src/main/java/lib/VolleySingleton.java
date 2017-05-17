8eaf1079a7ad0208637cae2ef39c7462d856aafd
João Fran <jotaf.daniel@gmail.com>
2017-05-17 13:41:49 -0300

    VolleySingleton documentada

--- app/src/main/java/lib/VolleySingleton.java
+++ app/src/main/java/lib/VolleySingleton.java
@@ -12,6 +12,11 @@ import com.android.volley.toolbox.Volley;
 
 /**
  * Created by joaofran on 26/04/17.
+ *
+ * Singleton criado para que haja unicidade da instância
+ * da RequestQueue e persistência no ciclo de vida das
+ * Activities
+ *
  */
 
 public class VolleySingleton {
@@ -19,24 +24,34 @@ public class VolleySingleton {
     private static Context mContext;
     private RequestQueue mRequestQueue;
 
-
     private VolleySingleton (Context context) {
         mContext = context;
         mRequestQueue = getRequestQueue();
     }
 
+    /**
+     * Retorna a instância atual do Singleton ou cria
+     * uma nova (caso nenhuma tenha sido instanciada)
+     */
     public static synchronized VolleySingleton getInstance (Context context) {
         if (mInstance == null)
             mInstance = new VolleySingleton(context);
         return mInstance;
     }
 
+    /**
+     * Retorna a instância atual da RequestQueue ou cria
+     * uma nova (caso nenhuma tenha sido instanciada)
+     */
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null)
             mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
         return mRequestQueue;
     }
 
+    /**
+     * Adiciona Request req à RequestQueue
+     */
     public <T> void addToRequestQueue (Request<T> req) {
         getRequestQueue().add(req);
     }
